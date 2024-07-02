26ae35ec0d9d8dbca7965215c5761d5845fa39bc
Gary Henderson <26419401+Gary-H9@users.noreply.github.com>
2024-07-02 14:53:03 +0000

    :construction: Add many files

--- ollamate/settings.py
+++ ollamate/settings.py
@@ -47,6 +47,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+PROJECT_NAME = "ollamate"
 
 # Application definition
 
@@ -95,12 +96,24 @@ WSGI_APPLICATION = 'ollamate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DB_HOST = os.environ.get("DB_HOST", "127.0.0.1")
+ENABLE_DB_SSL = (
+    str(os.environ.get("ENABLE_DB_SSL", DB_HOST not in ["127.0.0.1", "localhost"])).lower()
+    == "true"
+)
+DATABASES: dict = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", PROJECT_NAME),
+        "USER": os.environ.get("DB_USER", ""),
+        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "HOST": DB_HOST,
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
+
+if ENABLE_DB_SSL:
+    DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
 
 
 # Password validation
---
+++ scripts/devcontainer/post-create.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Upgrade NPM
+npm install --global npm@latest
+
+# Start Postgres
+docker compose --file contrib/docker-compose-postgres.yml up --detach
+
+# Upgrade Pip
+pip install --break-system-package --upgrade pip
+
+# Install dependencies
+pip install --break-system-package --requirement requirements.dev.txt
+
+# install npm dependencies and static assets
+# npm install
+# make build-static
+
+# Run migrations
+python manage.py migrate
