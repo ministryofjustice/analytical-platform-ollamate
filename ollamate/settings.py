b95be6cf2d67df429b0834692c620840c40536a6
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-07-25 14:34:43 +0000

    :recycle: Big Ol' Refactor

--- ollamate/settings.py
+++ ollamate/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list = ["127.0.0.1"]
+ALLOWED_HOSTS: list = []
 
 PROJECT_NAME = "ollamate"
 
